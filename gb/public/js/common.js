9f3272989b8155d25bb8762f3498455f12894563
JunilHwang <tjsdlf4261@naver.com>
2019-09-10 19:04:14 +0900

    B모듈 - 영상 편집 작업중

--- gb/public/js/common.js
+++ gb/public/js/common.js
@@ -2,7 +2,6 @@ const data = {
   loaded: false,
   selected: null,
   status: null,
-  ctx: null,
   video: null,
 }
 
@@ -31,34 +30,53 @@ const selectObject = e => {
   }
 }
 
+const twoNum = n => `0${n}`.substr(-2)
+
+const timeFormat = t => {
+  const ms = ~~(t * 100) % 100
+  const s = ~~t % 60
+  const m = ~~(t/60) % 60
+  const h = ~~(t/3600) % 24
+  return `${twoNum(h)}:${twoNum(m)}:${twoNum(s)}:${twoNum(ms)}`
+}
+
 const selectCover = e => {
   const target = e.currentTarget
   const [src, type] = [target.dataset.url, 'video/mp4']
   const wrap = $('.video-wrap')
-  const cvs = $('canvas', wrap)[0]
   const video = data.video = $('video', wrap)[0]
-  data.ctx = data.ctx || cvs.getContext('2d')
   $('source', video).attr({ src, type })
   $('.video-editor__object .active').removeClass('active')
   video.load()
-  const timer = () => {
-    if (video.paused || video.ended) return;
-    draw()
-    setTimeout(timer)
+  video.oncanplay = e => {
+    $('.none', wrap).hide()
+    data.loaded = true
+    $('#duration').html(timeFormat(video.duration))
   }
-  const draw = () => data.ctx.drawImage(video, 0, 0, 800, 450)    
-  video.addEventListener("play", timer)
-  video.addEventListener("canplay", e => {
-    $('.video-wrap .none').hide()
-    if (video.currentTime === 0) video.currentTime = 0.1
-    else {
-      data.loaded = true
-      draw()
-    }
-  })
+  video.ontimeupdate = e => {
+    $('#current').html(timeFormat(video.currentTime))
+  }
+}
+
+const draw = e => {
+  const video = data.video
+  const wrap = $('.video-wrap')
+  switch (e.type) {
+    case 'mousedown' :
+      const {pageX, pageY} = e
+      const {top, left} = $(video).offset()
+      const [x, y] = [pageX - left, pageY - top]
+    break
+    case 'mouseout' :
+    case 'mouseup' :
+    break
+    case 'mousemove' :
+    break
+  }
 }
 
 $(document)
   .on('click', 'a[href="#"]', _ => false)
   .on('click', '.video-editor__object a', selectObject)
   .on('click', '.teaser__cover a', selectCover)
+  .on('mousedown mouseup mouseout mousemove', '.video-wrap svg', draw)
