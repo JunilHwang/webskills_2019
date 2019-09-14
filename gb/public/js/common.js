6b9ae301254fa4cf7c8bc515bd4c320ee55b3028
junil-hwang <tjsdlf4261@naver.com>
2019-09-14 14:56:40 +0900

    B모듈 - 중간 리팩토링

--- gb/public/js/common.js
+++ gb/public/js/common.js
@@ -1,13 +1,5 @@
-const data = {
-  loaded: false,
-  selected: null,
-  status: null,
-  video: null,
-  weight: 3,
-  size: 16,
-  color: '#999',
-  clips: []
-}
+const data = { loaded: false, selected: null, status: null, video: null, weight: 3, size: 16, color: '#999', clips: [] }
+const setAttr = (target, attr) => { for (const key in attr) { target.setAttribute(key, attr[key]) } }
 const selectEvent = e => {
   e.preventDefault()
   if (data.loaded === false) {
@@ -16,7 +8,7 @@ const selectEvent = e => {
   }
   const $this = $(e.target)
   if ($this.hasClass('active')) return
-  const selected = data.objectType = e.target['id']
+  const selected = e.target['id']
   switch (true) {
     case selected === 'one' :
       const clip = $('.timeline li.active'), index = clip.index()
@@ -48,49 +40,31 @@ const selectEvent = e => {
   }
 }
 const twoNum = n => `0${n}`.substr(-2)
-const timeFormat = t => {
-  const ms = ~~(t * 100) % 100
-  const s = ~~t % 60
-  const m = ~~(t/60) % 60
-  const h = ~~(t/3600) % 24
-  return `${twoNum(h)}:${twoNum(m)}:${twoNum(s)}:${twoNum(ms)}`
-}
+const timeFormat = t => [~~(t/3600)%24, ~~(t/60)%60, ~~t%60, ~~(t*100)%100].map(v => twoNum(v)).join(':')
 const selectCover = e => {
-  const target = e.currentTarget
-  const [src, type] = [target.dataset.url, 'video/mp4']
-  const wrap = $('.video-wrap')
-  const video = data.video = $('video', wrap)[0]
-  $('source', video).attr({ src, type })
   $('.video-editor__object .active').removeClass('active')
+  const target = e.currentTarget
+  const wrap = $('.video-wrap')
+  const video = data.video = wrap.find('video')[0]
+  video.setAttribute('src', target.dataset.url)
   video.load()
-  video.oncanplay = e => {
-    $('.none', wrap).hide()
+  video.oncanplay = _ => {
+    wrap.find('.none').hide()
     data.loaded = true
     $('#duration').html(timeFormat(video.duration))
   }
-  video.ontimeupdate = e => {
-    $('#current').html(timeFormat(video.currentTime))
-  }
+  video.ontimeupdate = _ => $('#current').html(timeFormat(video.currentTime))
 }
-const selectOption = e => {
-  const {name, value} = e.target
-  data[name] = value
-}
+const selectOption = (e, {name, value} = e.target) => data[name] = value
 const drawEnd = (el, { duration } = data.video, start = 0, end = duration) => {
   data.clips.push({ start, end, duration, el })
-  timelineRender([...data.clips])
+  timelineRender(data.clips)
 }
 const Drawing = class {
-  constructor (target) {
-    this._target = document.createElementNS('http://www.w3.org/2000/svg', target)
-  }
+  constructor (target) { this._target = document.createElementNS('http://www.w3.org/2000/svg', target) }
   get () { return this._target }
-  set ({x, y}) {
-    const target = this._target
-    const {color, weight} = data
-    target.setAttribute('stroke', color)
-    target.setAttribute('stroke-width', weight)
-    target.setAttribute('fill', 'none')
+  set ({x, y}, target = this._target) {
+    setAttr(target, { 'stroke': data.color, 'stroke-width': data.weight, 'fill': 'none' })
     this._x = x, this._y = y
   }
   drawing () {}
@@ -98,67 +72,42 @@ const Drawing = class {
 }
 const Line = class extends Drawing {
   constructor ({x, y}) { super('path'); this.set({x, y}) }
-  set ({x ,y }) {
-    super.set({x, y})
-    const target = this._target
-    target.setAttribute('d', `M${x} ${y}`)
-  }
-  drawing ({x, y}) {
-    const {d} = this._target.attributes
-    d.value += `L${x} ${y} `
-  }
+  set ({x ,y }) { super.set({x, y}); this._target.setAttribute('d', `M${x} ${y}`) }
+  drawing ({x, y}) { this._target.attributes.d.value += `L${x} ${y} ` }
 }
 const Rect = class extends Drawing {
   constructor ({ x, y }) { super('rect'); this.set({x, y}) }
-  set ({ x, y }) {
+  set ({ x, y }, target = this._target) {
     super.set({x, y})
-    const target = this._target
-    target.setAttribute('x', x)
-    target.setAttribute('y', y)
+    setAttr(target, {x, y})
   }
-  drawing ({ x, y }) {
-    const {_x, _y, _target: target} = this
-    const width = x - _x, height = y - _y
-    if (width > 0) {
-      target.setAttribute('width', width)
-    } else {
-      target.setAttribute('width', -width), target.setAttribute('x', x)
-    }
-    if (height > 0) {
-      target.setAttribute('height', height)
-    } else {
-      target.setAttribute('height', -height), target.setAttribute('y', y)
-    }
+  drawing ({ x, y }, target = this._target) {
+    const width = x - this._x, height = y - this._y
+    const attr = {}
+    if (width < 0) attr.x = x
+    if (height < 0) attr.y = y
+    setAttr(target, {...attr, width: Math.abs(width), height: Math.abs(height)})
   }
   end () { super.end(); this._target.setAttribute('fill', data.color) }
 }
 const Text = class {
   constructor ({x, y}) {
-    const target = document.createElement('div')
-    const {size, color} = data
     this.x = x, this.y = y
-    target.setAttribute('contentEditable', true)
-    target.style.cssText = `position:absolute;left:${x}px;top:${y}px;font-size:${size}px;color:${color};z-index:50`
-    target.innerHTML = ''
+    const target = document.createElement('div')
+    setAttr(target, {
+      contentEditable: true,
+      style: `position:absolute;left:${x}px;top:${y}px;font-size:${data.size}px;color:${data.color};z-index:50`
+    })
     $(target)
       .on('keydown', e => { if (e.keyCode === 13) e.target.blur() })
-      .on('blur', e => {
-        target.remove()
-        this.end(e.target.innerHTML)
-      })
+      .on('blur', () => { target.remove(); this.end(target.innerHTML) })
       .prependTo('.video-wrap')
     setTimeout(() => target.focus())
   }
   end (text) {
     if (text.length === 0) return
     const target = document.createElementNS('http://www.w3.org/2000/svg', 'text')
-    const {size, color} = data
-    const {x, y} = this
-    target.setAttribute('dominant-baseline', 'hanging')
-    target.setAttribute('x', x)
-    target.setAttribute('y', y+3)
-    target.setAttribute('fill', color)
-    target.setAttribute('font-size', size)
+    setAttr(target, { 'dominant-baseline': 'hanging', 'x': this.x, 'y': this.y+3, 'fill': data.color, 'font-size': data.size })
     target.innerHTML = text
     $('.video-wrap svg').append(target)
     drawEnd(target)
@@ -167,31 +116,27 @@ const Text = class {
 const draw = (() => {
   let nowTarget = null
   return e => {
-    if ($('.draw.active').length === 0) return false
+    if ($('.draw.active').length === 0) return
     const { selected } = data
-    const wrap = $('.video-wrap')
+    const topWrap = $('.video-wrap .top')
     if (selected !== 'Text' && nowTarget && nowTarget.end && ['mouseout', 'mouseup'].indexOf(e.type) !== -1) {
       nowTarget.end()
       nowTarget = null
-      $('.top', wrap).removeClass('active')
-      return false
+      topWrap.removeClass('active')
+      return
     }
     const {pageX, pageY} = e
     const {top, left} = $(e.currentTarget).offset()
     const [x, y] = [pageX - left, pageY - top]
     switch (e.type) {
       case 'mousedown' :
-        if (['Line', 'Rect', 'Text'].indexOf(selected) !== -1) {
-          nowTarget = new ({Line, Rect, Text}[selected])({x, y})
-          if (selected !== 'Text') {
-            e.target.appendChild(nowTarget.get())
-            $('.top', wrap).addClass('active')
-          }
-        }
+        if (['Line', 'Rect', 'Text'].indexOf(selected) === -1) return
+        nowTarget = new ({Line, Rect, Text}[selected])({x, y})
+        if (selected === 'Text') return
+        e.target.appendChild(nowTarget.get())
+        topWrap.addClass('active')
       break
-      case 'mousemove' :
-        if (nowTarget && nowTarget.drawing) nowTarget.drawing({x, y})
-      break
+      case 'mousemove' : if (nowTarget && nowTarget.drawing) nowTarget.drawing({x, y}); break
     }
   }
 })();
@@ -236,40 +181,32 @@ const selectWrapper = (clip, shape) => {
   selectShapeReal(shape)
 }
 const moveShape = (() => {
-  let moving = false, x1, y1, target, moveChk = 0
+  let moving = false, x, y, target, moveChk = 0
   return e => {
-    const wrap = $('.video-wrap')
-    const {pageX: x, pageY: y} = e
-    const {beforeX, beforeY} = e.currentTarget.dataset
+    const moveWrap = $('.video-wrap .move')
+    const {pageX, pageY} = e
     switch (e.type) {
       case 'mousedown' :
-        $('.move', wrap).addClass('active')
-        moving = true, x1 = x - (beforeX || 0), y1 = y - (beforeY || 0), target = e.currentTarget
+        moveWrap.addClass('active')
+        target = e.currentTarget
+        const {beforeX, beforeY} = target.dataset
+        moving = true, x = pageX - (beforeX || 0), y = pageY - (beforeY || 0), 
         moveChk = target.getAttribute('transform')
       break
       case 'mousemove' :
-        if (moving) {
-          const moveX = x - x1, moveY = y - y1
-          target.setAttribute('transform', `translate(${moveX}, ${moveY})`)
-          target.setAttribute('data-before-x', moveX)
-          target.setAttribute('data-before-y', moveY)
-        }
+        if (!moving) return
+        const moveX = pageX - x, moveY = pageY - y
+        setAttr(target, { 'transform': `translate(${moveX}, ${moveY})`, 'data-before-x': moveX, 'data-before-y': moveY })
       break
       case 'mouseup' :
         moving = false
-        $('.move', wrap).removeClass('active')
-        if (moveChk === target.getAttribute('transform')) {
-          $(target).click()
-        }
+        moveWrap.removeClass('active')
+        if (moveChk === target.getAttribute('transform')) $(target).click()
       break
     }
   }
 })();
-const timelineRange = ({start, end, duration}) => {
-  const left = (start/duration) * 100
-  const width = ((end - start)/duration) * 100
-  return `left:${left}%;width:${width}%`
-}
+const timelineRange = ({start, end, duration}) => `left:${(start/duration) * 100}%;width:${((end - start)/duration) * 100}%`
 const timelineRender = arr => {
   $('.timeline').html(arr.map((v, k) => `
       <li data-key="${k}">
@@ -282,22 +219,20 @@ const resizeClip = (() => {
   return e => {
     const wrap = e.currentTarget
     const target = wrap.children[0]
-    const index = $(wrap).index()
     switch (e.type) {
       case 'mousemove' :
         let x = e.offsetX + target.offsetLeft
         if (beforeX < e.offsetX) x = e.offsetX
         beforeX = x
-        const {start, end, duration} = target.dataset
+        const {end, duration} = target.dataset
         const w = (end / duration) * 800
         if (!resizing) {
-          const chk = w - 20 < x && x < w
-          wrap.classList[chk ? 'add' : 'remove']('resizing')
+          wrap.classList[w-20 < x && x < w ? 'add' : 'remove']('resizing')
         } else {
           const end = (x / 800) * duration
           target.setAttribute('data-end', end)
-          target.style.cssText = timelineRange({ start, end, duration })
-          data.clips[index].end = end
+          target.style.cssText = timelineRange(target.dataset)
+          data.clips[$(wrap).index()].end = end
         }
       break;
       case 'mousedown' :
@@ -305,10 +240,9 @@ const resizeClip = (() => {
       break
       case 'mouseup' :
       case 'click' :
-        if (resizing) {
-          resizing = false
-          wrap.click()
-        }
+        if (!resizing) return
+        resizing = false
+        wrap.click()
       break
     }
   }
@@ -323,37 +257,25 @@ const moveClip = (() => {
     timeline = timeline || $('.timeline')
     switch (e.type) {
       case 'mouseenter' : 
-      case 'mouseleave' :
-        timeline.sortable('option', 'disabled', e.type === 'mouseenter')
-      break
+      case 'mouseleave' : timeline.sortable('option', 'disabled', e.type === 'mouseenter'); break
       case 'mousemove' :
         if (moving) {
           const moveX = ((ox - beforeX)/800) * duration
           const moveStart = start*1 + moveX
           const moveEnd = end*1 + moveX
           if (moveStart < 0 || moveEnd > duration) return
-          clip.start = moveStart
-          clip.end = moveEnd
-          target.setAttribute('data-start', moveStart)
-          target.setAttribute('data-end', moveEnd)
+          setAttr(target, { 'data-start': (clip.start = moveStart), 'data-end': (clip.end = moveEnd) })
           target.style.cssText = timelineRange(clip)
         }
       break
       case 'mousedown' :
-        if (ox < target.clientWidth - 20) {
-          moving = true
-          moved = clip.start
-          beforeX = ox
-        }
+        if (ox > target.clientWidth - 20) return
+        moving = true
+        moved = clip.start
+        beforeX = ox
       break
-      case 'mouseup' :
-        moving = false
-      break
-      case 'click' :
-        if (moved !== clip.start) {
-          e.stopPropagation()
-        }
-      break;
+      case 'mouseup' : moving = false; break
+      case 'click' : if (moved !== clip.start) e.stopPropagation(); break;
     }
   }
 })();
